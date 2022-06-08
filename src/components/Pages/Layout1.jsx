b191b34379feec49968e1ba0ba2686918377ab93
Subhiksha18vic <esubivictoria129@gmail.com>
2022-06-08 08:09:08 +0530

    Added images| Videos in data.json

---
+++ src/components/Pages/Layout1.jsx
@@ -0,0 +1,24 @@
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function Layout1(props) {
+  return (
+    <AnimatePresence>
+      <motion.div
+        id="page-content"
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 20 }}
+        exit={{ opacity: 0, y: -10 }}
+        transition={{ duration: 2.5 }}
+        className="container overflow-hidden"
+        style={{ paddingTop: "100px" }}
+      >
+        {props.children}
+
+        <div className="text-center mt-5 py-5" id="footer">
+          Built with ❤️ by
+          <br /> <span className="fw-bold">We Sigma</span>
+        </div>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
